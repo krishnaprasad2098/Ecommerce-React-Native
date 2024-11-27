4256caa3b0d2d165ef9afbadff5eaa368180a74a
krishnaprasad2098 <payamma.krishna@gmail.com>
2024-11-27 22:05:58 +0530

    inputfield,button types

---
+++ lib/types.ts
@@ -0,0 +1,21 @@
+import { TextInputProps, TouchableOpacityProps } from "react-native";
+
+export interface InputFieldProps extends TextInputProps {
+  label: string;
+  icon?: any;
+  secureTextEntry?: boolean;
+  labelStyle?: string;
+  containerStyle?: string;
+  inputStyle?: string;
+  iconStyle?: string;
+  className?: string;
+}
+
+export interface ButtonProps extends TouchableOpacityProps {
+  title: string;
+  bgVariant?: "primary" | "secondary" | "danger" | "outline" | "success";
+  textVariant?: "primary" | "default" | "secondary" | "danger" | "success";
+  IconLeft?: React.ComponentType<any>;
+  IconRight?: React.ComponentType<any>;
+  className?: string;
+}
