20ebae83f9346e1e3366fc190916720c4164e690
krishnaprasad2098 <payamma.krishna@gmail.com>
2024-11-27 22:06:47 +0530

    Add AppButton component

---
+++ components/global/AppButton.tsx
@@ -0,0 +1,62 @@
+import { ButtonProps } from "@/lib/types";
+import { TouchableOpacity, Text } from "react-native";
+
+const getBgVariantStyle = (variant: ButtonProps["bgVariant"]) => {
+  switch (variant) {
+    case "secondary":
+      return "bg-gray-500";
+    case "danger":
+      return "bg-red-500";
+    case "success":
+      return "bg-green-500";
+    case "outline":
+      return "bg-transparent border-neutral-300 border-2";
+    default:
+      return "bg-[#0286ff]";
+  }
+};
+
+const getTextVariantStyle = (variant: ButtonProps["textVariant"]) => {
+  switch (variant) {
+    case "primary":
+      return "text-black";
+    case "secondary":
+      return "text-gray-100";
+    case "danger":
+      return "text-red-100";
+    case "success":
+      return "text-green-100";
+
+    default:
+      return "text-white";
+  }
+};
+
+const AppButton = ({
+  onPress,
+  title,
+  bgVariant = "primary",
+  textVariant = "default",
+  IconLeft,
+  IconRight,
+  className,
+  ...props
+}: ButtonProps) => {
+  return (
+    <TouchableOpacity
+      onPress={onPress}
+      className={`rounded-full p-3 flex flex-row items-center justify-center shadow-md shadow-neutral-400/70 w-full ${getBgVariantStyle(
+        bgVariant
+      )} ${className}`}
+      {...props}
+    >
+      {IconLeft && <IconLeft />}
+      <Text className={`text-lg font-bold ${getTextVariantStyle(textVariant)}`}>
+        {title}
+      </Text>
+      {IconRight && <IconRight />}
+    </TouchableOpacity>
+  );
+};
+
+export default AppButton;
